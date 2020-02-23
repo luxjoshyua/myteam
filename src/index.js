454b3f90fdd8efc87f1a215c0cb25808893d368e
luxjoshyua <josh.e.fielding@gmail.com>
2020-02-23 12:57:09 +1100

    started tablet queries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import style from "./sass/index.scss";
+
+// check form has been completed 
+const form = document.getElementsByTagName('form');
+console.log(form);
+const formInputs = form.elements;
+console.log(formInputs);
+const submitBtn = document.querySelector('.submit-btn');
+console.log(submitBtn);
+
+const checkForm = () => {
+   
+    const canSubmit = true;
+
+    for (let i = 0; i < formInputs.length; i++) {
+        if (formInputs[i].value.length === 0) {
+            canSubmit = false;
+            formInputs[i].style.color = '#F67E7E';
+            formInputs[i].innerHTML = 'this field is required';
+        }
+    }
+    if (canSubmit) {
+        submitBtn.disabled = false; 
+    }
+}
+
+checkForm();

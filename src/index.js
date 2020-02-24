3151a1545da7a29d934b3cca0a8ed64742d8454f
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-02-24 13:19:34 +1100

    got pluses working

--- src/index.js
+++ src/index.js
@@ -29,14 +29,24 @@ import style from "./sass/index.scss";
 
 // show dialogue boxes
 const showBoxes = [...document.querySelectorAll(".expand")];
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
 
 showBoxes.forEach(box => {
+
     box.addEventListener('click', (e) => {
-        box.style.backgroundColor = "#79C8C7";
-        box.innerHTML = '-';
+
         box.parentElement.querySelector('.popup-box').classList.toggle('show');
+
+        if (box.parentElement.querySelector('.popup-box').classList.contains('show')) {
+            box.style.backgroundColor = "#79C8C7";
+            minus.style.display = "block";
+            plus.style.display = "none";
+        } else {
+            box.style.backgroundColor = "#F67E7E";
+            plus.style.display = "block";
+            minus.style.display = "none";
+
+        }
     })
 })
-
-
-

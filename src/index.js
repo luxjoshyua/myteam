4e6388ff6e81f7bfaa416e2a7d16d28e58aae7d5
luxjoshyua <josh.e.fielding@gmail.com>
2020-02-28 07:32:09 +1100

    added content fadein effect

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 import style from "./sass/index.scss";
 
+import "./js/content-fade";
+
 // check form has been completed 
-// const form = document.getElementsByTagName('form');
-// console.log(form);
-// const formInputs = form.elements;
-// console.log(formInputs);
-// const submitBtn = document.querySelector('.submit-btn');
-// console.log(submitBtn);
+const form = document.querySelector("form");
+const formInputs = form.elements;
+const submitBtn = document.querySelector('.submit-btn');
+const formErrorMessage = document.querySelectorAll('.error-message');
 
-// const checkForm = () => {
+const checkForm = () => {
 
-//     const canSubmit = true;
+    let canSubmit = true;
 
-//     for (let i = 0; i < formInputs.length; i++) {
-//         if (formInputs[i].value.length === 0) {
-//             canSubmit = false;
-//             formInputs[i].style.color = '#F67E7E';
-//             formInputs[i].innerHTML = 'this field is required';
-//         }
-//     }
-//     if (canSubmit) {
-//         submitBtn.disabled = false; 
-//     }
-// }
+    for (let index = 0; index < formInputs.length; index++) {
 
-// checkForm();
+        const element = formInputs[index];
+       
+
+        if (element.value.length === 0) {
+
+            console.log("It's an empty field")
+
+            element.style.border = "1px solid pink";
+
+            // formErrorMessage.style.display = "block";
+            
+            canSubmit = false;
+
+        } else if (element.value.length > 0) {
+            console.log("User has entered text into the field");
+            element.style.border = "none";
+        }
+    }
+
+    if (canSubmit) {
+        // ! means not
+        submitBtn.disabled = !canSubmit;
+    }
+
+}
+
+checkForm();
+
+
 
 
 
@@ -32,39 +50,39 @@ import style from "./sass/index.scss";
 
 
 // show dialogue boxes
-const showBoxes = [...document.querySelectorAll(".expand")];
-const plus = document.querySelector('.plus');
-const minus = document.querySelector('.minus');
+// const showBoxes = [...document.querySelectorAll(".expand")];
+// const plus = document.querySelector('.plus');
+// const minus = document.querySelector('.minus');
 
-showBoxes.forEach(box => {
+// showBoxes.forEach(box => {
 
-    box.addEventListener('click', (e) => {
+//     box.addEventListener('click', (e) => {
 
-        box.parentElement.querySelector('.popup-box').classList.toggle('show');
+//         box.parentElement.querySelector('.popup-box').classList.toggle('show');
 
-        if (box.parentElement.querySelector('.popup-box').classList.contains('show')) {
-            box.style.backgroundColor = "#79C8C7";
-            minus.style.display = "block";
-            plus.style.display = "none";
-        } else {
-            box.style.backgroundColor = "#F67E7E";
-            plus.style.display = "block";
-            minus.style.display = "none";
+//         if (box.parentElement.querySelector('.popup-box').classList.contains('show')) {
+//             box.style.backgroundColor = "#79C8C7";
+//             minus.style.display = "block";
+//             plus.style.display = "none";
+//         } else {
+//             box.style.backgroundColor = "#F67E7E";
+//             plus.style.display = "block";
+//             minus.style.display = "none";
 
-        }
-    })
-})
+//         }
+//     })
+// })
 
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile-nav-open');
-const mobileClose = document.querySelector('.close-icon');
+// const hamburger = document.querySelector('.hamburger');
+// const mobileNav = document.querySelector('.mobile-nav-open');
+// const mobileClose = document.querySelector('.close-icon');
 
-hamburger.addEventListener('click', (e) => {
-    mobileNav.classList.toggle('mobile-show');
-    mobileNav.classList.remove('mobile-close');
-})
+// hamburger.addEventListener('click', (e) => {
+//     mobileNav.classList.toggle('mobile-show');
+//     mobileNav.classList.remove('mobile-close');
+// })
 
-mobileClose.addEventListener('click', (e) => {
-    mobileNav.classList.toggle('mobile-close'); 
-    mobileNav.classList.remove('mobile-show');
-})
+// mobileClose.addEventListener('click', (e) => {
+//     mobileNav.classList.toggle('mobile-close'); 
+//     mobileNav.classList.remove('mobile-show');
+// })
---
+++ src/js/content-fade.js
@@ -0,0 +1,40 @@
+const animatedTags = document.querySelectorAll("h1, h2, p, img, h6");
+
+// fade out on load
+animatedTags.forEach(tag => {
+    tag.style.opacity = 0
+})
+
+const fadeIn = function () {
+
+    // let delay = 0.25
+    let delay = 0.5
+
+    animatedTags.forEach(tag => {
+        // the () runs it
+        const tagTop = tag.getBoundingClientRect().top
+        const tagBottom = tag.getBoundingClientRect().bottom
+
+        if (tagTop < window.innerHeight && tagBottom > 0) {
+            // keep the s because we want it to be a second
+            tag.style.animation = `fadeIn 1s ${delay}s both`
+            // delay = delay + 0.25
+            delay = delay + 0.05
+        } else {
+            tag.style.opacity = 0
+            tag.style.animation = ""
+        }
+    })
+}
+
+// on load, run fadeIn
+fadeIn()
+
+// on scroll, run fadeIn
+document.addEventListener("scroll", function () {
+    fadeIn()
+})
+
+window.addEventListener("resize", function () {
+    fadeIn()
+})
